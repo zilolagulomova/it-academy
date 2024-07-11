d4eec74d68158658db964993241ffea48b9502a7
zilolagulomova <gzilola__@mail.ru>
2024-07-11 22:02:27 +0500

    last commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 const menuBtn = document.querySelector(".menuBtn");
 const closeBtn = document.querySelector(".closeBtn");
 const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector(".menu");
 
 menuBtn.addEventListener("click", () => {
     hamburger.classList.remove("translate-x-full")
     hamburger.classList.add("translate-x-0")
+    closeBtn.classList.add('!block')
+    menuBtn.classList.add('!hidden')
+    document.body.style.overflow = "hidden"
+})
+
+closeBtn.addEventListener("click", () => {
+    hamburger.classList.remove("translate-x-0")
+    closeBtn.classList.remove('!block')
+    menuBtn.classList.remove('!hidden')
+    document.body.style.overflow = "auto"
+    header.classList.remove("justify-end")
+    header.classList.add("justify-between")
 })
